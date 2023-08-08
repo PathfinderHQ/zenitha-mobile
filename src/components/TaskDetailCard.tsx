fe86b2c3e977f0ea63096589fdde5746225eb021
Meng Kang <Mkang5798@conestogac.on.ca>
2023-08-08 16:57:31 -0400

    change background color

--- src/components/TaskDetailCard.tsx
+++ src/components/TaskDetailCard.tsx
@@ -6,6 +6,7 @@ import { alarm, Colors, FontSize } from '../constants';
 import { Task } from '../types';
 import BackButton from './BackButton';
 
+
 const { height, width } = Dimensions.get('screen');
 
 interface TaskDetailCardProps {
@@ -19,6 +20,7 @@ const TaskDetailCard: FC<TaskDetailCardProps> = ({ task, alarmIcon, customStyles
 
     return (
         <TouchableOpacity style={[styles.container, customStyles]}>
+            <BackButton/>
             <View style={styles.headerContainer}>
                 <Text style={styles.titleText}>{title}</Text>
                 <Text style={styles.titleText}>{dayjs(task.time).format('ddd, D MMM, YYYY')}</Text>
@@ -54,7 +56,7 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         padding: 10,
         borderRadius: 8,
-        backgroundColor: '#252525',
+        backgroundColor: '#E9ECEF',
     },
     headerContainer: {
         flex: 1,
@@ -84,21 +86,21 @@ const styles = StyleSheet.create({
     },
     titleText: {
         fontSize: FontSize.heading2Size,
-        color: Colors.white100,
+        color: Colors.main_text,
         marginTop: '5%',
         fontWeight: 'bold',
     },
     timeText: {
         fontSize: 28,
-        color: Colors.white100,
+        color: Colors.main_text,
     },
     descriptionText: {
         fontSize: 22,
-        color: Colors.white100,
+        color: Colors.main_text,
     },
     catText: {
         fontSize: 18,
-        color: Colors.white100,
+        color: Colors.main_text,
     },
     icon: {
         color: Colors.main_text,
@@ -110,7 +112,7 @@ const styles = StyleSheet.create({
         marginLeft: '20%',
     },
     greyText: {
-        color: Colors.placeholder,
+        color: Colors.unselectFieldStroke,
         fontSize: 24,
         marginBottom: '3%',
     },
